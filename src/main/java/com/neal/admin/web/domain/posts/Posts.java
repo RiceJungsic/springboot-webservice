d1cb77455c57e7e775449d54fff7f43afd050778
GreenIT-Neal <skgt5565@greenit.cc>
2024-07-23 10:20:08 +0900

    [ADD] : posts repository 진행중

---
+++ src/main/java/com/neal/admin/web/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.neal.admin.web.domain.posts;
+
+import com.neal.admin.web.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
